7d3dbc00246da84df1c3d94505cb2741713674c8
Liam Sutton <devliam90@gmail.com>
2020-03-04 09:55:40 +0000

    fix room ids

--- util/create_world.py
+++ util/create_world.py
@@ -6,13 +6,29 @@ Room.objects.all().delete()
 
 two_d_array = []
 
+tiles = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 5, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 6, 6, 0],
+    [5, 0, 0, 7, 5, 0, 7, 0, 0, 7, 0, 7, 0, 5, 0, 0, 0, 0, 0, 0],
+    [5, 0, 6, 6, 5, 0, 0, 0, 6, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [5, 0, 6, 6, 5, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0],
+    [5, 0, 0, 0, 5, 5, 0, 5, 5, 0, 0, 5, 5, 0, 0, 7, 0, 0, 0, 0],
+    [0, 6, 6, 6, 0, 6, 5, 0, 0, 0, 7, 0, 0, 7, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 6, 0, 6, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+]
+
 rows = 10
 cols = 20
+counter = 1
 
 for row in range(rows):
     temp_arr = []
     for col in range(cols):
-        temp_arr.append(Room(title=f'{row} {col}', description=f'{row} {col}'))
+        temp_arr.append(
+            Room(id=counter, title=f'{row} {col}', description=tiles[row][col]))
+        counter += 1
     two_d_array.append(temp_arr)
 
 
@@ -35,41 +51,5 @@ for row in range(rows):
 
 players = Player.objects.all()
 for p in players:
-    p.currentRoom = two_d_array[0][0].title
+    p.currentRoom = two_d_array[0][0].id
     p.save()
-
-# r_outside = Room(title="Outside Cave Entrance",
-#                  description="North of you, the cave mount beckons")
-
-# r_foyer = Room(title="Foyer", description="""Dim light filters in from the south. Dusty
-# passages run north and east.""")
-
-# r_overlook = Room(title="Grand Overlook", description="""A steep cliff appears before you, falling
-# into the darkness. Ahead to the north, a light flickers in
-# the distance, but there is no way across the chasm.""")
-
-# r_narrow = Room(title="Narrow Passage", description="""The narrow passage bends here from west
-# to north. The smell of gold permeates the air.""")
-
-# r_treasure = Room(title="Treasure Chamber", description="""You've found the long-lost treasure
-# chamber! Sadly, it has already been completely emptied by
-# earlier adventurers. The only exit is to the south.""")
-
-# r_outside.save()
-# r_foyer.save()
-# r_overlook.save()
-# r_narrow.save()
-# r_treasure.save()
-
-# # Link rooms together
-# r_outside.connectRooms(r_foyer, "n")
-# r_foyer.connectRooms(r_outside, "s")
-
-# r_foyer.connectRooms(r_overlook, "n")
-# r_overlook.connectRooms(r_foyer, "s")
-
-# r_foyer.connectRooms(r_narrow, "e")
-# r_narrow.connectRooms(r_foyer, "w")
-
-# r_narrow.connectRooms(r_treasure, "n")
-# r_treasure.connectRooms(r_narrow, "s")
